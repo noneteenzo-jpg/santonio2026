4ddb75cc3bfc174a42a420731b06a8ac0c07ab09
TERAK171 <noneteenzo@gmail.com>
2026-09-21 08:50:59 -0300

    att

--- Aulas php/aula 2  - php/atividade3.php
+++ Aulas php/aula 2  - php/atividade3.php
@@ -1,8 +1,34 @@
-<?php
-$peso = 70;
-$altura = 1.75;
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 2 - Exercício 2</title>
+</head>
+<body>
+    <h1>Calcular IMC</h1>
+    <form method="GET">
+        <label>Qual é seu peso? (em KG)</label>
+        <input type="number" name="peso" required>
 
-$imc = $peso / ($altura * $altura);
+        <br></br>
 
-echo $imc;
-?>
+        <label>Qual é sua altura? (em metros)</label>
+        <input type="float" name="altura" required>
+
+        <br></br>
+
+        <button type="submit">Calcular</button>
+
+    </form>
+    <?php 
+        if (isset($_GET["peso"]) && isset($_GET["altura"])){
+            $peso = $_GET["peso"];
+            $altura = $_GET["altura"];
+            $imc = round($peso / ($altura * $altura), 2);
+            
+        echo "<h2>Seu IMC é $imc</h2>";
+        }
+    ?>
+</body>
+</html>
