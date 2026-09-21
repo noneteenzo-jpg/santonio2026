1bcf90a3383053dab1d44d1e004df8086789ecd4
TERAK171 <noneteenzo@gmail.com>
2026-09-21 09:09:10 -0300

    att

--- Aulas php/aula 2  - php/atividade3.php
+++ Aulas php/aula 2  - php/atividade3.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula 2 - Exercício 2</title>
+    <title>Aula 2 - Exercício 3</title>
 </head>
 <body>
     <h1>Calcular IMC</h1>
